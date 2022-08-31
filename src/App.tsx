727f1327ff6c369086b3a09cb81f58a291e83392
Polyane Tuag <pollyportugues@hotmail.com>
2022-08-31 09:01:02 -0300

    feat: rendering components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+     <Form />
     </div>
   );
 }
