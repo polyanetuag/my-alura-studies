ac1360f9cd89fcaeb813e8fb83289cda4bb40ad6
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-05 08:55:18 -0300

    feat: Timer component

---
+++ src/components/Timer/index.tsx
@@ -0,0 +1,15 @@
+import Button from "../Button";
+import Clock from "./Clock";
+import style from "./Timer.module.scss";
+
+export default function Timer() {
+  return (
+    <div className={style.timer}>
+      <p className={style.title}>Escolha um card e inicie o cronometro</p>
+      <div className={style.clockWrapper}>
+        <Clock />
+      </div>
+      <Button>Começar!</Button>
+    </div>
+  );
+}
