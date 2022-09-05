1ba2185659e30dc2d7f223f1897c728cd4fdac50
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-05 08:55:49 -0300

    feat: rendering Timer component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Form from '../components/Form';
-import List from '../components/List';
-import style from './App.module.scss';
+import React from "react";
+import Form from "../components/Form";
+import List from "../components/List";
+import Timer from "../components/Timer";
+import style from "./App.module.scss";
 
 function App() {
   return (
     <div className={style.AppStyle}>
-     <Form />
-     <List />
+      <Form />
+      <List />
+      <Timer />
     </div>
   );
 }
