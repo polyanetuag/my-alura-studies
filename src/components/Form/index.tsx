76efdb67bbbf8964e6f994d3685b6179c483e246
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-07 18:38:56 -0300

    feat: controlling the form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import Button from "../Button";
-import style from './Form.module.scss';
+import style from "./Form.module.scss";
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00",
+  };
+
+  addTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="task">Adicione um novo estudo</label>
           <input
             type="text"
             name="task"
+            value={this.state.task}
+            onChange={(event) =>
+              this.setState({ ...this.setState, task: event.target.value })
+            }
             id="task"
             placeholder="O que você quer estudar?"
             required
@@ -22,15 +36,17 @@ class Form extends React.Component {
             type="time"
             step="1"
             name="time"
+            value={this.state.time}
+            onChange={(event) =>
+              this.setState({ ...this.state, time: event.target.value })
+            }
             id="time"
             min="00:00:00"
             max="01:30:00"
             required
           />
         </div>
-        <Button>
-          Adicionar
-        </Button>
+        <Button>Adicionar</Button>
       </form>
     );
   }
