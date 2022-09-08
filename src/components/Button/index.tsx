1b711244a2c39862067e5fd6799a2844e0609802
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-08 09:02:54 -0300

    feat: typing the button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-class Button extends React.Component<{children: string}> {
+class Button extends React.Component<{
+  children?: string;
+  type?: "button" | "submit" | "reset" | undefined;
+}> {
   render() {
+    const { type = "button" } = this.props;
     return (
-    <button 
-    className={style.button}>
-      {this.props.children}
+      <button type={type} className={style.button}>
+        {this.props.children}
       </button>
-    )
+    );
   }
 }
 
