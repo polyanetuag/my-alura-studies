897e84d703431ebdbb6f48f80bb8b792d3009925
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-08 09:03:51 -0300

    feat: typing parent element

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../components/Form";
 import List from "../components/List";
 import Timer from "../components/Timer";
+import { ITask } from "../types/task";
 import style from "./App.module.scss";
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
   return (
     <div className={style.AppStyle}>
-      <Form />
-      <List />
+      <Form setTasks={setTasks} />
+      <List tasks={tasks} />
       <Timer />
     </div>
   );
