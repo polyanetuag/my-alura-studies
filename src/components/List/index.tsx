23adae51a7d05dfdaaf5a0d138005bc42b3ca1b5
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-08 09:03:33 -0300

    feat: typing the form and clearing the code

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,28 +1,11 @@
-import React, { useState } from "react";
+import { ITask } from "../../types/task";
 import Item from "./Item";
-import style from './List.module.scss'
+import style from "./List.module.scss";
 
-function List() {
-  const [tasks, setTasks] = useState([
-    {
-      task: "React",
-      time: "02:00:00",
-    },
-    {
-      task: "Javascript",
-      time: "01:00:00",
-    },
-    {
-      task: "Typescript",
-      time: "03:00:00",
-    },
-  ]) 
+function List({ tasks }: { tasks: ITask[] }) {
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {
-        console.log('clicou', tasks)
-        setTasks([...tasks, {task: 'Github', time: '03:30:00'}]) 
-      }}>Estudos do dia</h2>
+      <h2>Estudos do dia</h2>
       <ul>
         {tasks.map((task, index) => (
           <Item key={index} {...task} />
