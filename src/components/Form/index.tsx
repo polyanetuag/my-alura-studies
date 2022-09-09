344af97e4a4a4576af087a3b2cc291bc4c7af393
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-09 17:46:31 -0300

    feat: controlling the selection in the form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import { ITask } from "../../types/task";
 import Button from "../Button";
 import style from "./Form.module.scss";
+import {v4 as uuidv4} from 'uuid'
 
 class Form extends React.Component<{
-  setTasks: React.Dispatch<
-    React.SetStateAction<
-      ITask[]
-    >
-  >;
+  setTasks: React.Dispatch<React.SetStateAction<ITask[]>>;
 }> {
   state = {
     task: "",
@@ -17,7 +14,11 @@ class Form extends React.Component<{
 
   addTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    this.props.setTasks(oldTasks => [ ...oldTasks, { ...this.state } ]);
+    this.props.setTasks((oldTasks) => [
+      ...oldTasks,
+      { ...this.state, selected: false, completed: false, id: uuidv4() },
+    ]);
+    this.setState({ task: "", time: "00:00" });
   }
 
   render() {
