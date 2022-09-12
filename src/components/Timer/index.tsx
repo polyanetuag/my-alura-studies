fe15808aabd7c26f1b58d571acbaafec0051d062
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-12 19:22:42 -0300

    feat: updating time for selected card

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
+import { timeForSeconds } from "../../common/utils/time";
+import { ITask } from "../../types/task";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
-export default function Timer() {
+interface ITimerProps {
+  selectedTask?: ITask | undefined;
+}
+
+export default function Timer({ selectedTask }: ITimerProps) {
+  const [time, setTime] = useState<number>();
+
+  useEffect(() => {
+    if (selectedTask) {
+      setTime(timeForSeconds(selectedTask.time));
+    }
+  }, [selectedTask]);
+
+   
   return (
     <div className={style.timer}>
       <p className={style.title}>Escolha um card e inicie o cronometro</p>
+      Tempo: {time}
       <div className={style.clockWrapper}>
         <Clock />
       </div>
       <Button>Começar!</Button>
     </div>
   );
-}
+} 
