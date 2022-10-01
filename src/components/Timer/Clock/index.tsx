6dd58ad4070f8ebcb6256620086d2058890ab034
Polyane Tuag <pollyportugues@hotmail.com>
2022-10-01 15:15:51 -0300

    feat: showing the watch

--- src/components/Timer/Clock/index.tsx
+++ src/components/Timer/Clock/index.tsx
@@ -1,13 +1,23 @@
 import style from './Clock.module.scss'
 
-export default function Clock() {
+interface IClockProps {
+  time: number | undefined
+}
+
+export default function Clock({time = 0}: IClockProps) {
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60 
+  const [minuteDec, minuteUni] = String(minutes).padStart(2, '0').split('')
+  const [secondDec, secondUni] = String(seconds).padStart(2, '0').split('')
+
   return (
     <>
-      <span className={style.clockNumber}>0</span>
-      <span className={style.clockNumber}>0</span>
+    <span className={style.clockNumber}>{minuteDec}</span>
+      <span className={style.clockNumber}>{minuteUni}</span>
       <span className={style.clockDivision}>:</span>
-      <span className={style.clockNumber}>0</span>
-      <span className={style.clockNumber}>0</span>
+      <span className={style.clockNumber}>{secondDec}</span>
+      <span className={style.clockNumber}>{secondUni}</span>
     </>
   );
 }
+
