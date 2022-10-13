80141bdb3db8ba076504f2880c7021f8806c7277
Polyane Tuag <pollyportugues@hotmail.com>
2022-10-13 18:57:42 -0300

    refactor: replacing class with function

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-class Button extends React.Component<{
-  children?: string;
-  type?: "button" | "submit" | "reset" | undefined,
+interface ButtonProps {
+  children?: React.ReactNode;
+  type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
-    return (
-      <button onClick={onClick} type={type} className={style.button}>
-        {this.props.children}
-      </button>
-    );
-  }
+}
+
+function Button({ children, type, onClick }: ButtonProps) {
+  return (
+    <button className={style.button} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
